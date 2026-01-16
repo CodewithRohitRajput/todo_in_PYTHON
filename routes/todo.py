732a627bcb8ed9ec4301336995265c3d9a70961e
CodewithRohitRajput <rohitsinghrajput2k24@gmail.com>
2026-01-16 18:57:40 +0530

    added PUT and DELETE routers

--- routes/todo.py
+++ routes/todo.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, HTTPException
-from bson import objectid
+from bson import ObjectId
 from config.mongodb import todo_collection
 from models.todo import todos
 
@@ -22,3 +22,25 @@ async def get_todos():
          todos.append(i)
 
     return todos
+
+@router.delete("/{id}")
+async def delete_todo(id : str):
+    todo = await todo_collection.find_one({"_id" : ObjectId(id)})
+    if not todo:
+        raise HTTPException(status_code=404, detail="Todo no found bruh")
+    t = todos(**todo)
+    result =   await todo_collection.delete_one({"_id" : ObjectId(id)})
+    return {"message " : f"todo deleted : {t.title}"}
+
+@router.put("/{id}")
+async def update_todo(id : str , todo : todos):
+    if not ObjectId.is_valid(id):
+        raise HTTPException(status_code=404, detail="Todo not found")
+
+    result = await todo_collection.update_one(
+        {"_id" : ObjectId(id)},
+        {"$set" : todo.dict()}
+    )
+
+    return {"message" : f"{todo.title} Updated"}
+
